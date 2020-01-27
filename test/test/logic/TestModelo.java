e409777f74bd6da1a2d40f7a8c474bf733c48ac0
max197 <ma.gomez1@uniandes.edu.co>
2020-01-27 16:59:51 -0500

    agsdg

--- test/test/logic/TestModelo.java
+++ test/test/logic/TestModelo.java
@@ -18,7 +18,7 @@ public class TestModelo {
 
 	public void setUp2() {
 		for(int i =0; i< CAPACIDAD;i++){
-			modelo.agregar(2);
+			modelo.agregar(i);
 		}
 	}
 
@@ -31,23 +31,63 @@ public class TestModelo {
 	@Test
 	public void testDarTamano() {
 		// TODO
+		setUp2();
+		assertEquals(100, modelo.darTamano());
 	}
 
 	@Test
 	public void testAgregar() {
 		// TODO Completar la prueba
+		
+		//Agrega un elemento despues de haber llenado la capacidad del arreglo
+		setUp2();
+		modelo.agregar(101);
+		assertEquals(101, modelo.darTamano());
+		
+		//Agrega un elemento al Array
+		modelo.agregar(1);
+		assertEquals(102, modelo.darTamano());
+		
+		//Agrega un duplicado del elemento anterior al Array
+		modelo.agregar(1);
+		assertEquals(103,modelo.darTamano());
+		
+
+		
 	}
 
 	@Test
 	public void testBuscar() {
 		setUp2();
 		// TODO Completar la prueba
+		//busca un elemento determinado en el Array
+		Integer buscado = modelo.buscar(3);
+		assertTrue("No encontro el elemento que era", buscado.compareTo(3)==0);
+		
+		//busca un elemento que no se debio haber encontrado
+		Integer buscado2 = modelo.buscar(101);
+		assertNull("No debio haber encontrado ese string", buscado2);
+		
+		Integer buscado3 = modelo.buscar(9);
+		assertNotNull("No debia ser null", buscado3);
+		
 	}
 
 	@Test
 	public void testEliminar() {
 		setUp2();
 		// TODO Completar la prueba
+		Integer eliminado = modelo.eliminar(4);
+		assertTrue("El tamaño del arreglo debería ser 99", modelo.darTamano()==99 );
+		assertTrue("No se eliminó el elemento correcto", eliminado.compareTo(4)==0);
+		
+		Integer eliminado2 = modelo.eliminar(1);
+		assertTrue("El tamaño del arreglo debería ser 98", modelo.darTamano()==98 );
+		assertTrue("No se eliminó el elemento correcto", eliminado2.compareTo(1)==0);
+		
+		Integer eliminado3 = modelo.eliminar(0);
+		assertTrue("El tamaño del arreglo debería ser 98", modelo.darTamano()==97 );
+		assertTrue("No se eliminó el elemento correcto", eliminado3.compareTo(0)==0);
 		
 	}
 
